dd6bef2dec7122954c29bab20ddec7d9f712b769
t-bli <1308250095@qq.com>
2023-08-23 17:40:43 +0900

    fix student sql

--- src/main/java/com/example/studteath/repository/Student004Repository.java
+++ src/main/java/com/example/studteath/repository/Student004Repository.java
@@ -5,6 +5,7 @@ import com.example.studteath.dao.UserMapper;
 import com.example.studteath.dto.*;
 import com.example.studteath.entity.Student;
 import com.example.studteath.entity.User;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -34,37 +35,9 @@ public class Student004Repository {
     public StudentInfo searchstudent(Student004InputDto inputDto) {
        StudentInfo studentInfo = new StudentInfo();
         Student student = new Student();
-        Long id = inputDto.getId();
-        Long userId = inputDto.getUserId();
-        String staffId = inputDto.getStaffId();
-        String name = inputDto.getName();
-        String age = inputDto.getAge();
-        String sex = inputDto.getSex();
-        String tel = inputDto.getTel();
-        String maileAddress =  inputDto.getMaileAddress();
-        String grade = inputDto.getGrade();
-        String studentClass = inputDto.getStudentClass();
-        String guardianTel = inputDto.getGuardianTel();
-        String guardianMaileAddress = inputDto.getGuardianMaileAddress();
-        String guardianName = inputDto.getGuardianName();
-        String disableFlag = inputDto.getDisableFlag();
-
+        BeanUtils.copyProperties(inputDto, student);
        LocalDateTime dateTime = LocalDateTime.now();
-        student.setAge(age);
-        student.setName(name);
-        student.setSex(sex);
-        student.setGrade(grade);
-        student.setStudentClass(studentClass);
-        student.setDisableFlag(disableFlag);
         student.setUpdateDate(dateTime);
-        student.setId(id);
-        student.setUserId(userId);
-        student.setStaffId(staffId);
-        student.setTel(tel);
-        student.setMaileAddress(maileAddress);
-        student.setGuardianTel(guardianTel);
-        student.setGuardianMaileAddress(guardianMaileAddress);
-        student.setGuardianName(guardianName);
         Integer integer = studentMapper.updateStudents(student);
         if (integer > 0 ){
             Student003InputDto student003InputDto = new Student003InputDto();
